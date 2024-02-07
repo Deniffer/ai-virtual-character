00ef35258dada565594eaa6336c8c071aa293935
Deniffer <deniffer.001@gmail.com>
2024-02-07 22:31:34 +0800

    [chores] some lint update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+        { 
+            protocol: 'https',
+            hostname: "oss-cdn.deniffer.com",
+            pathname: "**"
+         },
+         {
+            protocol: 'https',
+            hostname: "ai-virtual-character-deniffer.s3.ap-southeast-1.amazonaws.com",
+            pathname: "**"
+         }
+        ]
+    },
+};
 
 export default nextConfig;
